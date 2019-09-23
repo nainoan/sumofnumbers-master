00f3127f6f0fe80f01eff55f06b092387991a549
Nainoa Jorg <51459240+nainoan@users.noreply.github.com>
2019-09-22 21:02:54 -1000

    Initial commit

---
+++ sumofnumbers.js
@@ -0,0 +1,40 @@
+const data = [0, 2, 3, 9, 11, 1, 2, 2, 7, 6, 8, 14, 9, 6, 5, 6, 4, 4, 3];
+
+function sumFor(list) {
+    let sum41 = 0;
+    for(let num of list) {
+      sum41 += num;
+    }
+    return sum41;
+}
+
+console.log(sumFor(data));
+
+function sumWhile(list){
+    let sum41 = 0;
+    let i = 0;
+    while(i < list.length) {
+        sum41 += list[i];
+        i++;
+  }
+  return sum41;
+}
+
+console.log(sumWhile(data));
+
+function sumReccursion(list){
+    if(list.length === 0){
+      return 0;
+    }
+    else{
+        return list[0] + sumReccursion(list.slice(1, list.length));
+    }
+}
+
+console.log(sumReccursion(data));
+
+function sumTheSimpleWay(list){
+    return _.reduce(list, function(memo, num){return memo + num}, 0);
+}
+
+console.log(sumTheSimpleWay(data));
